d3e30a4d8f33c4fe1c7e6640b12546839f6cdc50
Maher Bisan <maherbisan95@gmail.com>
2020-06-15 15:08:18 +0300

    ,

--- partA/IntMatrix.cpp
+++ partA/IntMatrix.cpp
@@ -3,48 +3,56 @@
 
 using std::cout;
 using std::endl;
-using mtm::IntMatrix;
-using mtm::Dimensions;
-const Dimensions DIM1X1(1,1);
+const mtm::Dimensions DIM1X1(1,1);
 
+// const int** allocSpace(int** matrix , mtm::Dimensions dimension){ //TODO
 
-void IntMatrix::allocSpace(){
+//     matrix = new int* [dimension.getRow()];
+//     for(int i=0 ; i < dimension.getRow() ; i++){
+//         matrix[i] = new int[dimension.getCol()];
+//     }
+//     return matrix;
+// }
+
+void mtm::IntMatrix::allocSpace(){
 
     matrix = new int* [dimension.getRow()];
-    for(int i=0 ; i < dimension.getRow() ; i++){
+    for( int i=0 ; i < dimension.getRow() ; i++ ){
         matrix[i] = new int[dimension.getCol()];
     }
 }
 
-IntMatrix::IntMatrix(Dimensions d, int init__val ):dimension(d) , init_val(init__val){
+mtm::IntMatrix::IntMatrix(mtm::Dimensions d, int init__val ):dimension(d) , init_val(init__val){
     
     allocSpace();
-    for(int i=0 ; i < dimension.getRow() ; i++){
-        for( int j=0 ; j < dimension.getCol() ; j++){
+    // matrix = allocSpace(matrix,dimension) //TODO
+    for( int i=0 ; i < dimension.getRow() ; i++ ){
+        for( int j=0 ; j < dimension.getCol() ; j++ ){
             matrix[i][j] = init_val;
         }
     }
 }
-IntMatrix::IntMatrix(int scalar_val):dimension(DIM1X1),init_val(scalar_val){
+
+mtm::IntMatrix::IntMatrix(int scalar_val):dimension(1,1),init_val(scalar_val){
     allocSpace();
     matrix[0][0]=init_val;
 }
 
-IntMatrix IntMatrix::Identity(int dimension){
-    
-    Dimensions dim(dimension,dimension);
-    IntMatrix identity(dim);
-    for(int i=0 ; i <dimension ; i++){
+mtm::IntMatrix mtm::IntMatrix::Identity(int dimension){
+
+    mtm::Dimensions dim(dimension,dimension);
+    mtm::IntMatrix identity(dim);
+    for(int i=0 ; i < dimension ; i++){
         identity(i,i) = 1; //TODO check if it's OK to write  1 
     }
     return identity;
 }
 
-IntMatrix IntMatrix::transpose() const{
+mtm::IntMatrix mtm::IntMatrix::transpose() const{
 
     int t_row = dimension.getCol() , t_col = dimension.getRow() ;
-    Dimensions t_dimension(t_row,t_col);
-    IntMatrix t_matrix(t_dimension);
+    mtm::Dimensions t_dimension(t_row,t_col);
+    mtm::IntMatrix t_matrix(t_dimension);
     for(int i=0 ; i < t_row ; i++){
         for(int j=0 ; j < t_col ; j++){
             t_matrix(i,j) = matrix[j][i];
@@ -53,16 +61,32 @@ IntMatrix IntMatrix::transpose() const{
     return t_matrix;
 }
 
-int& IntMatrix::operator()(int row_val , int col_val){
+int& mtm::IntMatrix::operator()(int row_val , int col_val){
     return matrix[row_val][col_val];
 }
 
-int IntMatrix::operator()(int row_val , int col_val ) const{
+int mtm::IntMatrix::operator()(int row_val , int col_val ) const{
     int res = matrix[row_val][col_val];
     return res;
 }
 
-IntMatrix& IntMatrix::operator+=(const IntMatrix& b){
+mtm::IntMatrix mtm::IntMatrix::operator-() const{
+
+    mtm::IntMatrix tmp = *this; //TODO test it if this works !!
+    // for(int i=0 ; i < dimension.getRow()  ; i++){
+    //   for(int j=0 ; j < dimension.getCol()  ; j++){
+    //     tmp(i,j)*=-1;
+    //   }
+    // }
+    
+    for(mtm::IntMatrix::iterator it = tmp.begin() ;it !=tmp.end() ;++it ) { //TODO pick one
+        (*it) *= -1;
+    }
+    return tmp ;
+}
+
+
+mtm::IntMatrix& mtm::IntMatrix::operator+=(const mtm::IntMatrix& b){
 
     if ( dimension.getRow() > b.dimension.getRow() ) { // matrix a and b not from the same dimension which mean casting happened and dime of b is one i.e. 1x1
         for(int i=0 ; i <  dimension.getRow() ; i++){
@@ -80,35 +104,20 @@ IntMatrix& IntMatrix::operator+=(const IntMatrix& b){
     return *this;
 }
 
-/*
-IntMatrix IntMatrix::operator-() const{
 
-    IntMatrix tmp = *this; //TODO test it if this works !!
-    // for(int i=0 ; i < dimension.getRow()  ; i++){
-    //   for(int j=0 ; j < dimension.getCol()  ; j++){
-    //     tmp(i,j)*=-1;
-    //   }
-    // }
-    
-    for(IntMatrix::iterator it = tmp.begin() ;it !=tmp.end() ;++it ) { //TODO pick one
-        (*it) *= -1;
-    }
-    return tmp ;
-}
+// mtm::IntMatrix& mtm::IntMatrix::operator+=(const mtm::IntMatrix& b){
 
-IntMatrix& IntMatrix::operator+=(const IntMatrix& b){
-
-    if ( a.size() > b.size() ) { // matrix a and b not from the same dimension which mean casting happened and dime of b is one i.e. 1x1
-        for (IntMatrix::iterator it_a = this.begin() ; it_a != a.end() ; ++it_a) { 
-          *it_a += b(0,0);
-        }
-    }else{
-        for (IntMatrix::iterator it_a = this.begin() ,it_b = b.begin() ; it_a != a.end() ; ++it_a , ++it_b ) { 
-            *it_a += *it_b;
-        }
-    }
-    return *this;
-}
+//     if ( a.size() > b.size() ) { // matrix a and b not from the same dimension which mean casting happened and dime of b is one i.e. 1x1
+//         for (mtm::IntMatrix::iterator it_a = (*this).begin() ; it_a != a.end() ; ++it_a) { 
+//           *it_a += b(0,0);
+//         }
+//     }else{
+//         for (mtm::IntMatrix::iterator it_a = this->begin() ,it_b = b.begin() ; it_a != a.end() ; ++it_a , ++it_b ) { 
+//             *it_a += *it_b;
+//         }
+//     }
+//     return *this;
+// }
 
 // IntMatrix& operator+=(const int& num){
 //     for(IntMatrix::iterator it_a = this.begin() ; it_a != a.end() ; ++it_a) { 
@@ -117,26 +126,26 @@ IntMatrix& IntMatrix::operator+=(const IntMatrix& b){
 //     return *this;
 // }
 
-IntMatrix operator+(const IntMatrix& a ,const IntMatrix& b){
+mtm::IntMatrix operator+(const mtm::IntMatrix& a ,const mtm::IntMatrix& b){
     //if casting happened and dime of a or b is one i.e. 1x1  , this mean that the user want to add scalar
-    IntMatrix tmp_1 = a.size() >= b.size() ? a : b; 
-    IntMatrix tmp_2 = tmp_1.size() == a.size() ? b : a;
+    mtm::IntMatrix tmp_1 = a.size() >= b.size() ? a : b; 
+    mtm::IntMatrix tmp_2 = tmp_1.size() == a.size() ? b : a;
     return tmp_1 += tmp_2;
 }
 
 
-bool mtm::all(const IntMatrix& a){
+bool mtm::all(const mtm::IntMatrix& a){
 
-    for(IntMatrix::const_iterator it = a.begin() ;it !=a.end() ; ++it ) { 
+    for(mtm::IntMatrix::const_iterator it = a.begin() ;it !=a.end() ; ++it ) { 
         if(!(*it)){
             return false;
         }
     }
     return true;
 }
-bool mtm::any(const IntMatrix& a){
+bool mtm::any(const mtm::IntMatrix& a){
 
-    for(IntMatrix::const_iterator it = a.begin() ;it !=a.end() ; ++it ) { 
+    for(mtm::IntMatrix::const_iterator it = a.begin() ;it !=a.end() ; ++it ) { 
         if((*it)){
             return true;
         }
@@ -144,8 +153,6 @@ bool mtm::any(const IntMatrix& a){
     return false;
 }
 
-
-*/
 
 void print(int** matrix, int r , int c)
 {   
