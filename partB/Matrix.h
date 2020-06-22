0349262c6d9ddb33edc3c527de341f131b796ef0
Maher Bisan <maherbisan95@gmail.com>
2020-06-22 15:02:06 +0300

    matrix

---
+++ partB/Matrix.h
@@ -0,0 +1,19 @@
+#ifndef EX3_MATRIX_H
+#define EX3_MATRIX_H
+
+#include <iostream>
+
+#include "Auxiliaries.h"
+
+namespace mtm {
+
+    template < typename T >
+    class Matrix {
+       private:
+       
+       public:
+    };  // class Matrix
+
+}  // namespace mtm
+
+#endif  // EX3_MATRIX_H
